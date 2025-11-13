9325f932e87d4d53045bc75f6fde17016b1d5fb9
Sudhanshu Gaurav <sudhanshu10gaurav@gmail.com>
2025-11-13 17:58:45 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY', 'SUHANI' ],
+			strings: [ 'HAPPY', 'BIRTHDAY', 'NAME !' ],
 			charSize: 30,
 			charSpacing: 30,
 			lineHeight: 40,

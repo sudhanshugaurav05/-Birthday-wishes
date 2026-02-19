1cacac520a3e61c5fc1f5f35396a8c55956b77fe
Sudhanshu Gaurav <sudhanshu10gaurav@gmail.com>
2026-02-19 23:48:00 +0530

    Update birthday message in script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY', 'NAME !' ],
+			strings: [ 'HAPPY', 'BIRTHDAY', 'My Love !' ],
 			charSize: 30,
 			charSpacing: 30,
 			lineHeight: 40,
